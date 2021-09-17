4a11d2e84af42375dc723c7dbb1a432b8aba5014
stereobooster <stereobooster@gmail.com>
2021-09-17 02:13:00 +0200

    It works

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from "next/app";
+import { IdProvider } from "@radix-ui/react-id";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <IdProvider>
+      <Component {...pageProps} />
+    </IdProvider>
+  );
+}
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+import { useId } from "@radix-ui/react-id";
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  const id = useId();
+  console.log(id)
+  return <Title id={id}>My page</Title>;
 }
