bbbbd664d7f53cce0a6f8239bfc02507d5aab5da
stereobooster <stereobooster@gmail.com>
2021-09-17 11:35:20 +0200

    Ok, it's strict mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import { IdProvider } from "@radix-ui/react-id";
+import { StrictMode } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <IdProvider>
-      <Component {...pageProps} />
-    </IdProvider>
+    <StrictMode>
+      <IdProvider>
+        <Component {...pageProps} />
+      </IdProvider>
+    </StrictMode>
   );
 }
+
 export default MyApp;
